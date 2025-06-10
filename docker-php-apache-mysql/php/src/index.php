71fda4080ddf5dde04a2bfac7015376016b60c9d
TennyPH <tennyboyph@gmail.com>
2025-06-10 16:30:03 +0700

    10/06/2025  Update AppSheet

--- docker-php-apache-mysql/php/src/index.php
+++ docker-php-apache-mysql/php/src/index.php
@@ -3,38 +3,89 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="icon" href="image\Logo\Logo.png" type="image/png" />
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css" />
+    <title>Docker Test</title>
 </head>
-<body>
-    <?php
-       $host = 'db';
-       $user = 'MYSQL_USER';
-       $pass = 'MYSQL_PASSWORD';
-       $db = 'MYSQL_DATABASE';
 
-        $conn = new mysqli($host, $user, $pass, $db);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } else {
-            echo "Connected to MySQL server successfully!";
+<body>
+
+     <?php require_once 'nav.php'; ?>
+
+    <div class="sidebar">
+            <button class="active" onclick="showTab('tab1', this)">
+               <img src="image/Logo/home.png" class="icon" alt=" " />
+                    Home
+            </button>
+             <button onclick="showTab('tab2', this)">
+                <img src="image/Logo/Job.png" class="icon" alt=" " />
+                    Job
+            </button>
+
+            <button class=" " onclick="showTab('tab3', this)">
+                <img src="image/Logo/Job Graph.png" class="icon" alt=" " />
+                    Job Graph
+            </button>
+
+            <button onclick="showTab('tab4', this)">
+                <img src="image/Logo/Inventory Graph.png" class="icon" alt=" " />
+                    Inventory Graph
+            </button>
+
+            <button onclick="showTab('tab5', this)">
+                <img src="image/Logo/All Job.png" class="icon" alt=" " />
+                    All Job
+            </button>
+
+            <button onclick="showTab('tab6', this)">
+                <img src="image/Logo/IS Round67.png" class="icon" alt=" " />
+                    IS Round67
+            </button>
+
+            <button onclick="showTab('tab7', this)">
+                <img src="image/Logo/Report IS Round.png" class="icon" alt=" " />
+                    Report IS Round
+            </button>
+
+            <button onclick="showTab('tab8', this)">
+                <img src="image/Logo/About.png" class="icon" alt=" " />
+                    About
+            </button>
+
+            <button onclick="showTab('tab9', this)">
+                <img src="image/Logo/App Gallery.png" class="icon" alt=" " />
+                    App Gallery
+            </button>
+        </div>
+            <!-- <div class="content">
+                <div id="tab1" class="tabcontent active">
+                    <h2>หน้าแรก</h2>
+                    <p>นี่คือเนื้อหาของหน้าแรกที่แสดงโดยไม่โหลดหน้าใหม่</p>
+                </div>
+            <div id="tab2" class="tabcontent">
+                <h2>รายการ</h2>
+                <p>นี่คือเนื้อหาของแท็บรายการ</p>
+            </div>
+            <div id="tab3" class="tabcontent">
+            <h2>ตั้งค่า</h2>
+            <p>นี่คือเนื้อหาของแท็บตั้งค่า</p>
+            </div>
+        </div>
+
+    <script>
+        function showTab(tabId, btn) {
+            document.querySelectorAll('.tabcontent').forEach(tab => tab.classList.remove('active'));
+            document.getElementById(tabId).classList.add('active');
+
+            document.querySelectorAll('.sidebar button').forEach(b => b.classList.remove('active'));
+            btn.classList.add('active');
+            
         }
 
-            $sql = "SELECT * FROM users";
-
-            if ($result = $conn->query($sql)){
-                while($data = $result->fetch_object()){
-                    $users[] = $data;
-                }
-            }
-            
-            echo "<ul>";
-            foreach($users as $user) {
-                echo "<li>";
-                echo $user->first_name . " " . $user->last_name . " " . $user->age;
-                echo "</li>";
-            }
-
-            echo "</ul>"; 
-    ?>
+    </script>
+     -->
 </body>
+
 </html>
---
+++ docker-php-apache-mysql/php/src/nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #184f8c;">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">
+      <img src="image\Logo\Logo.png" alt=" " style="width:40px;" class="rounded-pill"> 
+      </a>
+      <a class="navbar-brand" href="#">HelpDesk.062566</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+      <div class="collapse navbar-collapse justify-content-center">
+        <form class="container">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        </form>
+      </div>
+       <button class="btn btn-success" type="submit">Login</button>
+    </div>
+  </div>
+</nav>
