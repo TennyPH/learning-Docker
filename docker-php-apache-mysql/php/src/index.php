75a7793c5eecf8a8a16d888c63eb093d78ee08ff
TennyPH <tennyboyph@gmail.com>
2025-06-11 16:30:31 +0700

    Update 11.6.68

--- docker-php-apache-mysql/php/src/index.php
+++ docker-php-apache-mysql/php/src/index.php
@@ -8,71 +8,76 @@
     <script src="js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="style.css" />
     <title>Docker Test</title>
+    
 </head>
 
 <body>
 
      <?php require_once 'nav.php'; ?>
+    <div class="app-layout d-flex">
+        <div class="sidebar">
+                <button class="active" onclick="showTab('tab1', this)">
+                <img src="image/Logo/home.png" class="icon" alt=" " />
+                        Home
+                </button>
+                <button onclick="loadTabContent('tab2', this)">
+                    <img src="image/Logo/Job.png" class="icon" alt=" " />
+                        Job
+                </button>
 
-    <div class="sidebar">
-            <button class="active" onclick="showTab('tab1', this)">
-               <img src="image/Logo/home.png" class="icon" alt=" " />
-                    Home
-            </button>
-             <button onclick="showTab('tab2', this)">
-                <img src="image/Logo/Job.png" class="icon" alt=" " />
-                    Job
-            </button>
+                <button class=" " onclick="showTab('tab3', this)">
+                    <img src="image/Logo/Job Graph.png" class="icon" alt=" " />
+                        Job Graph
+                </button>
 
-            <button class=" " onclick="showTab('tab3', this)">
-                <img src="image/Logo/Job Graph.png" class="icon" alt=" " />
-                    Job Graph
-            </button>
+                <button onclick="showTab('tab4', this)">
+                    <img src="image/Logo/Inventory Graph.png" class="icon" alt=" " />
+                        Inventory Graph
+                </button>
 
-            <button onclick="showTab('tab4', this)">
-                <img src="image/Logo/Inventory Graph.png" class="icon" alt=" " />
-                    Inventory Graph
-            </button>
+                <button onclick="showTab('tab5', this)">
+                    <img src="image/Logo/All Job.png" class="icon" alt=" " />
+                        All Job
+                </button>
 
-            <button onclick="showTab('tab5', this)">
-                <img src="image/Logo/All Job.png" class="icon" alt=" " />
-                    All Job
-            </button>
+                <button onclick="showTab('tab6', this)">
+                    <img src="image/Logo/IS Round67.png" class="icon" alt=" " />
+                        IS Round67
+                </button>
 
-            <button onclick="showTab('tab6', this)">
-                <img src="image/Logo/IS Round67.png" class="icon" alt=" " />
-                    IS Round67
-            </button>
+                <button onclick="showTab('tab7', this)">
+                    <img src="image/Logo/Report IS Round.png" class="icon" alt=" " />
+                        Report IS Round
+                </button>
 
-            <button onclick="showTab('tab7', this)">
-                <img src="image/Logo/Report IS Round.png" class="icon" alt=" " />
-                    Report IS Round
-            </button>
+                <button onclick="showTab('tab8', this)">
+                    <img src="image/Logo/About.png" class="icon" alt=" " />
+                        About
+                </button>
 
-            <button onclick="showTab('tab8', this)">
-                <img src="image/Logo/About.png" class="icon" alt=" " />
-                    About
-            </button>
+                <button onclick="showTab('tab9', this)">
+                    <img src="image/Logo/App Gallery.png" class="icon" alt=" " />
+                        App Gallery
+                </button>
+         </div>
 
-            <button onclick="showTab('tab9', this)">
-                <img src="image/Logo/App Gallery.png" class="icon" alt=" " />
-                    App Gallery
-            </button>
-        </div>
-            <!-- <div class="content">
-                <div id="tab1" class="tabcontent active">
-                    <h2>หน้าแรก</h2>
-                    <p>นี่คือเนื้อหาของหน้าแรกที่แสดงโดยไม่โหลดหน้าใหม่</p>
-                </div>
-            <div id="tab2" class="tabcontent">
-                <h2>รายการ</h2>
-                <p>นี่คือเนื้อหาของแท็บรายการ</p>
+        <div class="content bg-white rounded shadow-sm p-4">
+           
+            <div id="tab1" class="tabcontent active">
+                    <h2>Home</h2>
+                    <br>
+                    <p></p>
+            </div>
+            <!-- <div id="tab2" class="tabcontent">
+                    <h2>รายการ</h2>
+                    <p>นี่คือเนื้อหาของแท็บรายการ</p>
             </div>
             <div id="tab3" class="tabcontent">
-            <h2>ตั้งค่า</h2>
-            <p>นี่คือเนื้อหาของแท็บตั้งค่า</p>
-            </div>
+                    <h2>ตั้งค่า</h2>
+                    <p>นี่คือเนื้อหาของแท็บตั้งค่า</p>
+            </div> -->
         </div>
+    </div>
 
     <script>
         function showTab(tabId, btn) {
@@ -85,7 +90,7 @@
         }
 
     </script>
-     -->
+    
 </body>
 
 </html>
